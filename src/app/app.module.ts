95f2f2bd81a622d8c62d18d50f33e4f772e6fde1
mipdevp code <mipdevp@gmail.com>
2021-06-14 09:48:27 +0700

    Initial Template Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent,
-    ChildComponent
   ],
   imports: [
     BrowserModule,
