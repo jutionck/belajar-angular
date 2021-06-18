f7acb99da7ec8f4ac0913880b800ad12a3084dcd
Jution Kirana <mipdevp@gmail.com>
2021-06-18 08:40:36 +0700

    Implement asyncPipe

--- src/app/pages/components/todo/todo-list/todo-list.component.ts
+++ src/app/pages/components/todo/todo-list/todo-list.component.ts
@@ -9,6 +9,18 @@ export class TodoListComponent implements OnInit {
 
   sampleDate: Date = new Date();
   sampleDate2 = '12 Juni 2021';
+  sampleData: Promise<String> = Promise.resolve('sample data with promise')
+
+  promise: Promise<any>;
+  constructor() {
+    this.promise = this.getPromise(); (2)
+  }
+
+  getPromise() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve("Promise complete!"), 3000);
+    });
+  }
 
   ngOnInit(): void {
   }
