e15cc42c55dd7ec19ac0d11e017b368a8bae5def
Jution Kirana <mipdevp@gmail.com>
2021-07-02 09:39:34 +0700

    How to deploy angular with docker and nginx

--- src/app/pages/users/components/list/list-user.component.ts
+++ src/app/pages/users/components/list/list-user.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { Response } from "src/app/shared/models/response.model";
 import { User } from "../../model/user.model";
 import { UserService } from "../../service/user.service";
 
@@ -11,36 +9,17 @@ import { UserService } from "../../service/user.service";
 export class ListUserComponent implements OnInit {
   users: User[] = [];
   page = 0;
-  totalPage = 0;
 
   constructor(
-    private readonly activatedRoute: ActivatedRoute,
     private readonly userService: UserService
   ) { }
 
   ngOnInit(): void {
-    // query string/params setelah tanda ?,
-    // contoh: ?page=1&search=
-    this.activatedRoute.queryParams
-      .subscribe((queryParams: Params) => {
 
-        this.page = queryParams.page || 1;
-
-        this.userService.getAll(this.page)
-          .subscribe(
-            ({ total_pages, data: users }: Response<User[]>) => {
-              console.log(users);
-
-              this.users = users;
-              this.totalPage = total_pages;
-            });
+    this.userService.getAll()
+      .subscribe((users: User[]) => {
+        console.log(users);
+        this.users = users;
       });
-
-    // path variable biasanya muncul setelah /,
-    // contoh: list/12345
-    // this.activatedRoute.params
-    //   .subscribe((params: Params) => {
-    //     console.log('path variable:', params);
-    //   });
   }
 }
--- src/app/pages/users/service/user.service.ts
+++ src/app/pages/users/service/user.service.ts
@@ -8,41 +8,18 @@ import { User } from "../model/user.model";
 @Injectable()
 export class UserService {
 
-  apiUrl: string = 'https://reqres.in/api';
+  apiUrl: string = 'http://localhost';
 
   constructor(
     private readonly http: HttpClient
   ) { }
 
-  getAll(page: number = 1): Observable<Response<User[]>> {
-    return this.http.get<Response<User[]>>(`https://reqres.in/api/users?page=${page}`)
-
-      // Demo CORS
-      // return this.http
-      //   .get<any>(`${environment.apiBaseUrl}/premium/courses`)
-      .pipe(retry(3));
-
-    //   return new Observable((observer: Observer<Response<User[]>>) => {
-    //     const request = fetch(`https://reqres.in/api/users?page=${page}`);
-    //     request
-    //       .then((response) => {
-    //         return response.json();
-    //       })
-    //       .then((data) => {
-    //         observer.next(data);
-    //       })
-    //       .catch((error) => {
-    //         observer.error(error);
-    //       })
-    //   })
-    //     .pipe(
-    //       tap(() => console.log('request users')),
-    //       retry(5)
-    //     )
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/api/users`)
   }
 
   save(user: User): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/users`, user)
+    return this.http.post<User>(`${this.apiUrl}/api/users`, user)
       .pipe(retry(3));
   }
 }
